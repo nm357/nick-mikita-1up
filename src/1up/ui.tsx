9310f1ba37919b28849edc923c080b20dab2606f
n m <nm357@users.noreply.github.com>
2020-08-30 20:21:11 -0400

    init with ui placeholder

---
+++ src/1up/ui.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Ui extends React.Component {
+  render() {
+    return(
+      <div id="ui-container">
+        1up UI
+      </div>
+    )
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Ui from './1up/ui';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Ui />
   </React.StrictMode>,
   document.getElementById('root')
 );
