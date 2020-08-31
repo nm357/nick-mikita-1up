707caf0b53ae650d8909ab350b64cbaa983c70b9
n m <nm357@users.noreply.github.com>
2020-08-31 19:19:10 -0400

    wip retrieve all fhir bundles
    Is this like the $everything result? the 1up.health site has been 502 for me for a few hours now, so I can't check the docs.

--- server/index.js
+++ server/index.js
@@ -3,13 +3,30 @@ const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 const fetch = require('isomorphic-fetch');
 
+const { getAllFhirResourceBundles } = require('../src/1up/oneup');
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(pino);
 
+const PORT = 3001;
 const client_id = process.env.CLIENT_ID || 'id';
 const client_secret = process.env.CLIENT_SECRET || 'id';
+
+const token_url = 'https://api.1up.health/fhir/oauth2/token'; 
+const api_url = 'https://api.1up.health/fhir';  
+const scope = 'user/*.*';  
+
+// Create AppUser
+// Get AppUser Access Code
+// Exchange code for tokens
+
+let auth = {
+  code: undefined,
+  access_token: undefined,
+  refresh_token: undefined
+};
 
 app.post('/api/code', async (req, res) => {
   const appUserId = req.body.appUserId;
@@ -18,6 +35,7 @@ app.post('/api/code', async (req, res) => {
     .then(res => res.json())
     .then(json => { 
       console.log('json response from 1up', json);
+      auth.code = json.code;
       return json 
     })
     .catch(err => res.send(`Error in express fetch: ${err}`));
@@ -31,20 +49,78 @@ app.post('/api/token', async (req, res) => {
   console.log('code in server: ', code);
 
   res.setHeader('Content-Type', 'application/json');
-  const postUrl = `https://api.1up.health/fhir/oauth2/token?client_id=${client_id}&client_secret=${client_secret}&code=${code}&grant_type=authorization_code`;
+  const postUrl = `${token_url}?client_id=${client_id}&client_secret=${client_secret}&code=${code}&grant_type=authorization_code`;
   console.log('url: ', postUrl);
   
   const apiResonse = await fetch(postUrl, { method: 'POST' })
     .then(res => res.json())
     .then(json => { 
       console.log('json response from 1up', json);
+      auth.access_token = json.access_token;
+      auth.refresh_token = json.refresh_token;
       return json 
     })
     .catch(err => res.send(`Error in express fetch: ${err}`));
 
-   res.send(apiResonse);
+  res.send(apiResonse);
 });
 
-app.listen(3001, () =>
-  console.log('Express server is running on localhost:3001')
+// FHIR Api $everything query
+// To query everything simply hit this endpoint with the patient's id, fhir version type (dstu2/stu3/r4) and their bearer access token.
+
+// curl -X GET 'https://api.1up.health/fhir/{fhirVersion}/Patient/{patient_id}/$everything' \
+//   -H "Authorization: Bearer accesstokenaccesstoken"
+
+app.post('/api/fhir', async (req, res) => {
+  const system_id = '​4706'; // Epic demo
+  // const access_token = '996cd2f6bca74878a7f2bfe658f17597';
+  const access_token = req.body.code || 'accessCode';
+  console.log('auth: ', auth);
+  // const url = `${api_url}/${system_id}?access_token=${access_token}`;
+  const url = 'https://quick.1up.health/connect/4706?access_token=996cd2f6bca74878a7f2bfe658f17597'
+
+  const apiResonse = await fetch(url, { method: 'GET' })
+    .then(res => {
+      console.log('res', res);
+      return res;
+    })
+    // .then(res => res.json())
+    // .then(json => { 
+    //   console.log('json response from 1up', json);
+    //   return json 
+    // })
+    .catch(err => res.send(`Error in express fetch: ${err}`));
+  
+  res.send(apiResonse);
+});
+
+app.post('/api/fhir/everything', async (req, res) => {
+  console.log('$everything');
+  const access_token = req.body.code || 'accessCode';
+  const patient_id = req.body.patient_id || 'patientId';
+  const fhirVersion = 'DSTU2';
+  const url = `https://api.1up.health/fhir/${fhirVersion}/Patient/${patient_id}/$everything`
+
+  try {
+    // const apiResponse = await fetch(url, { method: 'GET', headers: {'Authorization': `Bearer ${access_token}`} })
+    //   .then(res => { console.log(res); })
+    //   .then(res => res.json)
+    //   .then(json => json)
+    //   .catch(err => console.log('err', err));
+  
+      const bundles = await new Promise((res, rej) => {
+        getAllFhirResourceBundles(access_token, (data) => res(data));
+      }).then(data => data); 
+  
+      console.log('bundles', bundles);
+  
+      // res.send(apiResonse);
+      res.send(bundles);
+  } catch (error) {
+    res.send({error});
+  }
+});
+
+app.listen(PORT, () =>
+  console.log('Express server is running on localhost:', PORT)
 );
--- src/1up/OneUpApp.jsx
+++ src/1up/OneUpApp.jsx
@@ -12,13 +12,14 @@ export default class OneUpApp extends React.Component {
     }
 
     this.handleClick = this.handleClick.bind(this);
-    this.getToken = this.getToken.bind(this);
   }
 
   handleClick(event) {
     event.preventDefault();
     // this.getToken('4f53f2cb34724277a61f2bcc11d70d36');
-    this.getCode('user_id');
+    // this.getFhirAuth('996cd2f6bca74878a7f2bfe658f17597');
+    this.getFhirEverything('306341f4490a4b94ac6b1bc8ea8c14f8');
+    // this.getCode('1');
   }
 
   async getCode(appUserId) {
@@ -48,12 +49,52 @@ export default class OneUpApp extends React.Component {
     this.setState({apiResponse: JSON.stringify(json)});
   }
 
+  async getFhirAuth(accessCode) {
+    const response = await fetch(`/api/fhir`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      // body: `code=${accessCode}`
+      body: `code=996cd2f6bca74878a7f2bfe658f17597`
+    });
+
+    const json = await response.json();
+
+    console.log('response from express',);
+    console.log('response from express', json);
+    // this.setState({apiResponse: JSON.stringify(json)});
+  }
+
+  async getFhirEverything(accessCode) {
+    const patientId = '1d5e078b47ba';
+    const response = await fetch(`/api/fhir/everything`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      body: `code=${accessCode}&patient_id=${patientId}`
+    });
+
+    console.log(response);
+    const json = await response.json();
+
+    console.log('response from express', json);
+    this.setState({apiResponse: JSON.stringify(json)});
+  }
+
   render() {
     return(
       <div id="ui-container">
         1up UI
 
         <button onClick={this.handleClick}>Get Code</button>
+        <div>
+          QuickConnect
+          <button onClick={this.handleClick}>Connect</button>
+        </div>
+
+        <div>
+          $everything
+          <button onClick={this.handleClick}>Connect</button>
+        </div>
+
         <div>
           api response:
           <div>
